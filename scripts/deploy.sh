32d7b0df854479ddce380be54e40f84adbca4439
Kevin Price <kevin.price@HIdigital.solutions>
2020-01-08 15:59:53 -0800

    Push zip files to azure

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,3 +20,22 @@ echo Deploy Infrastructure
 terraform apply -auto-approve
 
 echo Deploying functions
+az login --service-principal \
+  -u ${ARM_CLIENT_ID} \
+  --password=${ARM_CLIENT_SECRET} \
+  --tenant ${ARM_TENANT_ID}
+
+echo Zipping deployment files
+cd "EmoticonPublisher/bin/Release/"
+
+rm -f EmoticonPublisher.zip
+pushd netcoreapp3.0
+    zip -r ../EmoticonPublisher.zip .
+popd
+
+echo Deploying functions to Azure
+az functionapp deployment source config-zip \
+    -g "emoticon_service" \
+    -n "EmoticonPublisher" \
+    --subscription "${ARM_SUBSCRIPTION_ID}" \
+    --src EmoticonPublisher.zip
